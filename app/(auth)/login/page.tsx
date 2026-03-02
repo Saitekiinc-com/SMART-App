5da6fb0b389cc76643332809afa8b9fad51f475f
ryout0817 <kamiryo.0817@gmail.com>
2026-03-03 03:10:24 +0900

    Googleログイン追加、loginページ更新

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,10 +2,30 @@
  * S-01: ログイン / サインアップ
  * AuthForm を配置
  */
+import { signIn } from "@/auth";
+
+async function handleLogin(formData: FormData) {
+  "use server";
+  await signIn("credentials", formData);
+}
+
+async function handleGoogleLogin() {
+  "use server";
+  await signIn("google", { redirectTo: "/" });
+}
+
 export default function LoginPage() {
   return (
     <main>
-      <p>ログイン / サインアップ（S-01）</p>
+      <form action={handleLogin}>
+        <input name="email" type="email" />
+        <input name="password" type="password" />
+        <button type="submit">ログイン</button>
+      </form>
+
+      <form action={handleGoogleLogin}>
+        <button type="submit">Google でログイン</button>
+      </form>
     </main>
   );
 }
